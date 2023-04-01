c8a59e16ae29f107ba1d84f2612fd33df52905f7
Alastair Gilles <agilles91@gmail.com>
2023-04-01 05:15:06 +0200

    Refactor assembly building
    Use a computation expression to make the building of
    assembly appear closer to writing actual assembly.

--- src/AssemblyBuilder/AssemblyBuilder.fs
+++ src/AssemblyBuilder/AssemblyBuilder.fs
@@ -5,15 +5,54 @@ open System
 open SharpSilver.AST
 
 module Assembly =
+    let rec updateLast l update =
+        match l with
+        | [] -> []
+        | head::[] -> [update head]
+        | head::tail -> head :: updateLast tail update
+
     type Builder() =
         member _.Yield(_) = []
+
+        [<CustomOperation("SECTION")>]
+        member _.Section(l, name) =
+            l @ [sprintf "SECTION %s%s" name Environment.NewLine]
+
         [<CustomOperation("mov")>]
         member _.Mov(l, a, b) =
-            l @ [sprintf "    mov %A, %A" a b]
+            l @ [sprintf "    mov %O, %O" a b]
 
-        //member _.Run(l) = String.concat Environment.NewLine l
+        [<CustomOperation("xor")>]
+        member _.Xor(l, a, b) =
+            l @ [sprintf "    xor %O, %O" a b]
+        
+        [<CustomOperation("idiv")>]
+        member _.Idiv(l, a) =
+            l @ [sprintf "    idiv %O" a]
+
+        [<CustomOperation("syscall")>]
+        member _.SysCall(l) =
+            l @ [sprintf "    syscall"]
+
+        [<CustomOperation("label")>]
+        member _.Label(l, a) =
+            l @ [sprintf "_%s:" a]
+
+        [<CustomOperation("op2")>]
+        member _.Op2(l, name, a, b) =
+            l @ [sprintf "    %s %O, %O" name a b]
+
+        [<CustomOperation("comment")>]
+        member _.Comment(l, comment) =
+            updateLast l (fun s -> sprintf "%s ;%s" s comment)
+
+        member _.Run(l) = String.concat Environment.NewLine l
 
 let assemblyBuilder = new Assembly.Builder()
+
+let rax = "rax"
+let rdi = "rdi"
+let rdx = "rdx"
 
 let comp = assemblyBuilder {
     mov "rax" 1
@@ -22,51 +61,47 @@ let comp = assemblyBuilder {
 }
 
 let AddSumCode name operation a b =
-    [
-        sprintf "SECTION .text"
-        sprintf ""
-        sprintf "_%s:" name
-        sprintf "    mov rax, %d" a
-        sprintf "    %s rax, %d" operation b
-        sprintf "    mov rdi, rax"
-        sprintf "    mov rax, 60 ;sys_exit"
-        sprintf "    syscall"
-    ]
-    |> String.concat Environment.NewLine
+    assemblyBuilder {
+        SECTION ".text"
+
+        label name
+        mov rax a
+        op2 operation rax b
+        mov rdi rax
+        mov rax 60 ;comment "sys_exit"
+        syscall
+    }
 
 let DivCode name a b =
-    [
-        sprintf "SECTION .text"
-        sprintf ""
-        sprintf "_%s:" name
-        sprintf "    mov rax, %d" a
-        sprintf "    mov rdi, %d" b
-        sprintf "    xor rdx, rdx"
-        sprintf "    idiv rdi"
-        sprintf "    mov rdi, rax"
-        sprintf "    mov rax, 60 ;sys_exit"
-        sprintf "    syscall"
-    ]
-    |> String.concat Environment.NewLine
+    assemblyBuilder {
+        SECTION ".text"
+
+        label name
+        mov rax a
+        mov rdi b
+        xor rdx rdx
+        idiv rdi
+        mov rdi rax
+        mov rax 60 ;comment "sys_exit"
+        syscall
+    }
 
 let buildAssembly program =
     match program.EntryPoint.Body with
     | ReturnValue value ->
-        [
-            sprintf "SECTION .text"
-            sprintf ""
-            sprintf "_%s:" program.EntryPoint.Signature.Name
-            sprintf "    mov rdi, %d" value
-            sprintf "    mov rax, 60 ;sys_exit"
-            sprintf "    syscall"
-        ]
-        |> String.concat Environment.NewLine
+        assemblyBuilder {
+            SECTION ".text"
+
+            label program.EntryPoint.Signature.Name
+            mov rdi value
+            mov rax 60 ;comment "sys_exit"
+            syscall
+        }
     | FunctionCall {Function=name; Inputs=inputs} ->
         let a = inputs.[0]
         let b = inputs.[1]
-        let operation = name |> function
-            | "+" -> AddSumCode program.EntryPoint.Signature.Name "add" a b
-            | "-" -> AddSumCode program.EntryPoint.Signature.Name "sub" a b
-            | "/" -> DivCode program.EntryPoint.Signature.Name a b
-            | _ -> "ARG"
-        operation
+        match name with
+        | "+" -> AddSumCode program.EntryPoint.Signature.Name "add" a b
+        | "-" -> AddSumCode program.EntryPoint.Signature.Name "sub" a b
+        | "/" -> DivCode program.EntryPoint.Signature.Name a b
+        | _ -> "ARG"
